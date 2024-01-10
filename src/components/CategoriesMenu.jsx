3ed00b1c24eefb94cb0251de81ac65feb9148417
md nabab sharif <mdnababsharifdiu@gmail.com>
2024-01-10 23:53:09 +0600

    Categories Menu Update Done

--- src/components/CategoriesMenu.jsx
+++ src/components/CategoriesMenu.jsx
@@ -1,17 +1,34 @@
+import { useEffect, useState } from "react"
 import List from "./utilities/List"
 import ListItem from "./utilities/ListItem"
+import axios from 'axios'
+
 
 const CategoriesMenu = () => {
+  let [categoryData, setCategoryData] = useState();
+
+  useEffect(() => {
+    let response = async () => {
+      let getData = await axios.get('https://dummyjson.com/products/categories')
+      setCategoryData(getData.data)
+    }
+    response();
+  }, [])
+
+
+
   return (
     <>
       <div className="w-[263px] bg-[#262626] absolute left-0 mt-5">
-        <List className="">
-          <ListItem className="font-dm text-[14px] font-medium leading-normal text-[rgba(255,255,255,0.70)] hover:text-white  transition-all duration-400 capitalize py-4 pl-5 border-b-[1px] border-[#2D2D2D] cursor-pointer hover:pl-[30px] hover:font-bold last:border-[transparent] " text="Accesories" />
-          <ListItem className="font-dm text-[14px] font-normal leading-normal text-[rgba(255,255,255,0.70)] hover:text-white  transition-all duration-400 capitalize py-4 pl-5 border-b-[1px] border-[#2D2D2D] cursor-pointer hover:pl-[30px] hover:font-bold last:border-[transparent] " text="Electronice" />
-          <ListItem className="font-dm text-[14px] font-normal leading-normal text-[rgba(255,255,255,0.70)] hover:text-white  transition-all duration-400 capitalize py-4 pl-5 border-b-[1px] border-[#2D2D2D] cursor-pointer hover:pl-[30px] hover:font-bold last:border-[transparent] " text="Clothes" />
-          <ListItem className="font-dm text-[14px] font-normal leading-normal text-[rgba(255,255,255,0.70)] hover:text-white  transition-all duration-400 capitalize py-4 pl-5 border-b-[1px] border-[#2D2D2D] cursor-pointer hover:pl-[30px] hover:font-bold last:border-[transparent] " text="Bags" />
-          <ListItem className="font-dm text-[14px] font-normal leading-normal text-[rgba(255,255,255,0.70)] hover:text-white  transition-all duration-400 capitalize py-4 pl-5 border-b-[1px] border-[#2D2D2D] cursor-pointer hover:pl-[30px] hover:font-bold last:border-[transparent] " text="Home appliances" />
-          <ListItem className="font-dm text-[14px] font-normal leading-normal text-[rgba(255,255,255,0.70)] hover:text-white  transition-all duration-400 capitalize py-4 pl-5 border-b-[1px] border-[#2D2D2D] cursor-pointer hover:pl-[30px] hover:font-bold last:border-[transparent] " text="Furniture" />
+        <List className="h-[500px] overflow-y-scroll">
+
+          {categoryData?.map((item, index) => (
+
+            <ListItem key={index} className="font-dm text-[14px] font-medium leading-normal text-[rgba(255,255,255,0.70)] hover:text-white  transition-all duration-400 capitalize py-4 pl-5 border-b-[1px] border-[#2D2D2D] cursor-pointer hover:pl-[30px] hover:font-bold last:border-[transparent] " text={item} />
+
+          ))
+
+          }
         </List>
       </div>
     </>
