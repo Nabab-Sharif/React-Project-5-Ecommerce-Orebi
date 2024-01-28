3d5a3ca64315034e84b91200a0f70534d8cb6267
md nabab sharif <mdnababsharifdiu@gmail.com>
2024-01-28 21:32:19 +0600

    SeparteFile Product data  of NewArrivails Part

---
+++ src/components/dummyData/newArrivalsProduct.jsx
@@ -0,0 +1,68 @@
+import product1 from '../../../src/assets/images/newArrivalsImg1.png'
+import product2 from '../../../src/assets/images/newArrivalsImg2.png'
+import product3 from '../../../src/assets/images/newArrivalsImg3.png'
+import product4 from '../../../src/assets/images/newArrivalsImg4.png'
+
+
+
+
+let newArrivalsProduct = [
+  {
+    id: 0,
+    productName: "Basic Crew Neck Tee",
+    productPrice: 44,
+    productImage: product1,
+    isNew: "true",
+    discount: "false",
+    color: "black"
+  },
+  {
+    id: 1,
+    productName: "Watch",
+    productPrice: 340,
+    productImage: product2,
+    isNew: "false",
+    discount: 15,
+    color: 'false'
+  },
+  {
+    id: 2,
+    productName: "Basic Crew Neck Tee",
+    productPrice: 54,
+    productImage: product3,
+    isNew: "true",
+    discount: "false",
+    color: 'red'
+  },
+  {
+    id: 3,
+    productName: "Basic Crew Neck Tee",
+    productPrice: 74,
+    productImage: product4,
+    isNew: "false",
+    discount: 20,
+    color: 'false'
+  }
+  ,
+  {
+    id: 4,
+    productName: "Basic Crew Neck Tee",
+    productPrice: 54,
+    productImage: product2,
+    isNew: "false",
+    discount: 20,
+    color: 'false'
+  }
+  ,
+  {
+    id: 5,
+    productName: "Basic Crew Neck Tee",
+    productPrice: 94,
+    productImage: product3,
+    isNew: "false",
+    discount: 20,
+    color: 'false'
+  }
+]
+
+export default newArrivalsProduct
--- src/components/home/NewArrivals.jsx
+++ src/components/home/NewArrivals.jsx
@@ -1,9 +1,5 @@
 import ProductCart from '../product/ProductCart';
 import SectionTitle from './../utilities/SectionTitle';
-import productImage1 from '../../../src/assets/images/newArrivalsImg1.png'
-import productImage2 from '../../../src/assets/images/newArrivalsImg2.png'
-import productImage3 from '../../../src/assets/images/newArrivalsImg3.png'
-import productImage4 from '../../../src/assets/images/newArrivalsImg4.png'
 import { HiArrowNarrowLeft } from "react-icons/hi";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -13,71 +9,10 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-
+import dummyProduct from '../../components/dummyData/newArrivalsProduct'
 
 
 const NewArrivals = () => {
-
-  let allProduct = [
-    {
-      id: 0,
-      productName: "Basic Crew Neck Tee",
-      productPrice: 44,
-      productImage: productImage1,
-      isNew: "true",
-      discount: "false",
-      color: "black"
-    },
-    {
-      id: 1,
-      productName: "Watch",
-      productPrice: 340,
-      productImage: productImage2,
-      isNew: "false",
-      discount: 15,
-      color: 'false'
-    },
-    {
-      id: 2,
-      productName: "Basic Crew Neck Tee",
-      productPrice: 54,
-      productImage: productImage3,
-      isNew: "true",
-      discount: "false",
-      color: 'red'
-    },
-    {
-      id: 3,
-      productName: "Basic Crew Neck Tee",
-      productPrice: 74,
-      productImage: productImage4,
-      isNew: "false",
-      discount: 20,
-      color: 'false'
-    }
-    ,
-    {
-      id: 4,
-      productName: "Basic Crew Neck Tee",
-      productPrice: 54,
-      productImage: productImage2,
-      isNew: "false",
-      discount: 20,
-      color: 'false'
-    }
-    ,
-    {
-      id: 5,
-      productName: "Basic Crew Neck Tee",
-      productPrice: 94,
-      productImage: productImage3,
-      isNew: "false",
-      discount: 20,
-      color: 'false'
-    }
-  ]
-
-
 
 
 
@@ -103,7 +38,7 @@ const NewArrivals = () => {
             >
 
               {
-                allProduct.map((item, index) => (
+                dummyProduct.map((item, index) => (
                   <SwiperSlide key={index} >
                     <ProductCart productImage={item.productImage} productName={item.productName} productPrice={item.productPrice} discount={item.discount} isNew={item.isNew} color={item.color} id={item.id} />
                   </SwiperSlide>
